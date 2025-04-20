48dcf86a95bd2fde6a09bc8bf15c164f41584872
Celio Junior <cxliobj@outlook.com>
2025-04-20 20:43:02 -0300

    Board and piece initial implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,11 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        Position pos = new Position(0, 0);
-        Position pos2 = new Position(0, 1);
-        Position pos3 = new Position(1, 0);
-
-        System.out.println(pos);
-        System.out.println(pos2);
-        System.out.println(pos3);
+        Board board = new Board(8, 8);
     }
 }
