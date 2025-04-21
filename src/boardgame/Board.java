c19588b9239641947b396b2f219f73aaccf1e34c
Celio Junior <cxliobj@outlook.com>
2025-04-20 21:12:45 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -32,4 +32,14 @@ public class Board
     {
         this.cols = cols;
     }
+
+    public Piece piece(int row, int col)
+    {
+        return pieces[row][col];
+    }
+
+    public Piece piece(Position pos)
+    {
+        return pieces[pos.getRow()][pos.getCol()];
+    }
 }
